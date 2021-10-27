ced2f210d4917dd7be4ce9f2d9e54112394be4ba
KazeroG <17680194+KazeroG@users.noreply.github.com>
2021-10-26 21:09:19 -0400

    edit css and add pages

--- index.php
+++ index.php
@@ -1,6 +1,43 @@
-<html>
-
+<!DOCTYPE html>
+<html xmlns="https://www.w3.org/1999/xhtml" dir="ltr" lang="fr" xml:lang="fr">
 <head>
+
+	<title> </title>
+	<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=0">
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<meta http-equiv="Cache-control" content="public">
+	<meta name="title" content="">
+	<meta name="description" content="">
+	<meta name="keywords" content="allowebs,webs,web design,agence,web,web developement,dev,agency,web development,dev">
+	<meta name="author" content="Adryan Serage - AlloWebs CEO">
+
+	<!-- Open Graph / Facebook -->
+	<meta property="og:type" content="website">
+	<meta property="og:url" content="">
+	<meta property="og:title" content="">
+	<meta property="og:description" content="">
+	<meta property="og:image" content="https://allowebs.com/img/reg.png">
+	<meta property="og:image:alt" content="allowebs logo">
+	<meta property="fb:app_id" content="143174700465982">
+
+	<!-- Twitter -->
+	<meta property="twitter:card" content="summary_large_image">
+	<meta property="twitter:url" content="https://allowebs.com/">
+	<meta property="twitter:title" content="">
+	<meta property="twitter:description" content="">
+	<meta property=" twitter:image" content="/img/reg.png">
+
+	<meta name="theme-color" content="#FF0000">
+
+	<link rel="canonical" href="https://allowebs.com/">
+	<link rel="alternate" hreflang="en" href="https://en.allowebs.com/" title="english language version">
+	<link rel="alternate" hreflang="fr" href="https://fr.allowebs.com/" title="version langue francaise">
+
+	<link rel="apple-touch-icon" sizes="180x180" href="img/red.png" alt="allowebs logo">
+	<link rel="icon" type="image/png" sizes="32x32" href="img/red-icon.png" alt="allowebs logo">
+	<link rel="icon" type="image/png" sizes="16x16" href="img/white-icon.png" alt="allowebs logo">
+	<link rel="mask-icon" href="img/favicon/safari-pinned-tab.svg" color="#FF0000">
+
 	<link rel="stylesheet" href="style.css">
 </head>
 
@@ -17,12 +54,12 @@
 			<ul>
 				<li><a href="#">VIP</a></li>
 				<li><a href="#">50%</a></li>
-				<li><a href="#">R�gions</a>
+				<li><a href="#">Régions</a>
 					<div style="z-index:5"><br>
-						<a href="#">Montr�al</a><br>
+						<a href="#">Montréal</a><br>
 						<a href="#">Laval</a><br>
 						<a href="#">terrebonne</a><br>
-						<a href="#">Longueil</a><br>
+						<a href="#">Longueuil</a><br>
 						<a href="#">Varennes</a><br>
 					</div>
 				</li>
@@ -40,18 +77,55 @@
 	</div>
 
 	<div id="pm_media_section">
-		<div id="pm_media_content">
-			<video style="width:100%; height:100%;object-fit:fill" poster="images/vignette.png"
+		<div id="pm_media_content"
+
+		style="
+
+			position: absolute;
+  top: 0;
+  bottom: 0;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+
+			">
+			<video style="/* Make video to at least 100% wide and tall */
+  min-width: 100%;
+  min-height: 100%;
+
+  /* Setting width & height to auto prevents the browser from stretching or squishing the video */
+  width: auto;
+  height: auto;
+
+  /* Center the video */
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%,-50%);
+
+			" poster="images/vignette.png"
 				onclick="this.paused ? this.play() : this.pause();">
 				<source src="media/videoplayback.mp4" type="video/webm">
-				Votre navigateur ne permet pas de lire les vid�os HTML5.
+				Votre navigateur ne permet pas de lire les vidéos HTML5.
 			</video>
 		</div>
 	</div>
 
 	<div class="pm_horiz_separator"></div>
 	<div class="pm_horiz_container pm_content_center">
-		<a class="pm_button">Choisie ta r�gion</a>
+<div id="pm_menu" class="pm_header_section">
+			<ul>
+				<li><a href="#" class="pm_button">Régions</a>
+					<div style="z-index:5"><br>
+						<a href="#">Montréal</a><br>
+						<a href="#">Laval</a><br>
+						<a href="#">terrebonne</a><br>
+						<a href="#">Longueuil</a><br>
+						<a href="#">Varennes</a><br>
+					</div>
+				</li>
+			</ul>
+		</div>
 	</div>
 
 	<div class="pm_horiz_container pm_content_center"
@@ -164,6 +238,7 @@
 		<image src="images/mobile_app.png" />
 	</div>
 	<div id="pm_page_footer">
+
 	</div>
 </body>
 
---
+++ vip.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html xmlns="https://www.w3.org/1999/xhtml" dir="ltr" lang="fr" xml:lang="fr">
+<head>
+
+	<title> </title>
+	<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=0">
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<meta http-equiv="Cache-control" content="public">
+	<meta name="title" content="">
+	<meta name="description" content="">
+	<meta name="keywords" content="allowebs,webs,web design,agence,web,web developement,dev,agency,web development,dev">
+	<meta name="author" content="Adryan Serage - AlloWebs CEO">
+
+	<!-- Open Graph / Facebook -->
+	<meta property="og:type" content="website">
+	<meta property="og:url" content="">
+	<meta property="og:title" content="">
+	<meta property="og:description" content="">
+	<meta property="og:image" content="https://allowebs.com/img/reg.png">
+	<meta property="og:image:alt" content="allowebs logo">
+	<meta property="fb:app_id" content="143174700465982">
+
+	<!-- Twitter -->
+	<meta property="twitter:card" content="summary_large_image">
+	<meta property="twitter:url" content="https://allowebs.com/">
+	<meta property="twitter:title" content="">
+	<meta property="twitter:description" content="">
+	<meta property=" twitter:image" content="/img/reg.png">
+
+	<meta name="theme-color" content="#FF0000">
+
+	<link rel="canonical" href="https://allowebs.com/">
+	<link rel="alternate" hreflang="en" href="https://en.allowebs.com/" title="english language version">
+	<link rel="alternate" hreflang="fr" href="https://fr.allowebs.com/" title="version langue francaise">
+
+	<link rel="apple-touch-icon" sizes="180x180" href="img/red.png" alt="allowebs logo">
+	<link rel="icon" type="image/png" sizes="32x32" href="img/red-icon.png" alt="allowebs logo">
+	<link rel="icon" type="image/png" sizes="16x16" href="img/white-icon.png" alt="allowebs logo">
+	<link rel="mask-icon" href="img/favicon/safari-pinned-tab.svg" color="#FF0000">
+
+	<link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+
+	<div id="pm_page_header">
+		<div class="pm_header_section">
+			<span
+				style="background-color:#e08;color:#fff;background-image:url('images/pm.png');background-repeat:no-repeat;background-position:center"
+				class="pm_dot"></span>
+		</div>
+		<div id="pm_menu" class="pm_header_section">
+			<ul>
+				<li><a href="#">VIP</a></li>
+				<li><a href="#">50%</a></li>
+				<li><a href="#">Régions</a>
+					<div style="z-index:5"><br>
+						<a href="#">Montréal</a><br>
+						<a href="#">Laval</a><br>
+						<a href="#">terrebonne</a><br>
+						<a href="#">Longueuil</a><br>
+						<a href="#">Varennes</a><br>
+					</div>
+				</li>
+				<li><a href="#">FR </a>|<a href="#">&nbsp;EN</a> </li>
+
+			</ul>
+		</div>
+		<div class="pm_header_section">
+			<span class="pm_dot"></span>
+			<span class="pm_dot"></span>
+			<span class="pm_dot"></span>
+			<span class="pm_dot"></span>
+			<span class="pm_dot"></span>
+		</div>
+	</div>
+	<div id="pm_page_footer">
+
+	</div>
+</body>
+
+</html
